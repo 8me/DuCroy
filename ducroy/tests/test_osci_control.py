779b6df5f40e5e9cf23649d420bb7d8e97ec6ea4
Tamas Gal <tgal@km3net.de>
2018-04-24 14:19:52 +0200

    Add tests for resource manager

--- ducroy/tests/test_osci_control.py
+++ ducroy/tests/test_osci_control.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python
 
 import unittest
-from unittest.mock import patch
-from ducroy.osci_control import Osci
+from unittest.mock import patch, MagicMock
+from ducroy.osci_control import Osci, OPEN_CMD
 
 
 class TestOsci(unittest.TestCase):
     @patch('visa.ResourceManager')
-    def test_init(self, rm):
-        osci = Osci('1')
-        rm.assert_called_with("@py")
+    def test_init(self, rm_mock):
+        osci = Osci('1')  # noqa
+        rm_mock.assert_called_with("@py")
+
+    @patch('visa.ResourceManager')
+    def test_open_resource(self, rm_mock):
+        ip = '1'
+        osci = Osci(ip)
+        osci.rm = MagicMock()
+        osci._open_resource()
+        osci.rm.open_resource.assert_called_with(OPEN_CMD.format(ip))
