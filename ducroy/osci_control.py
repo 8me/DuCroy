62c24ff8baaf4f0143117b95c5855d4378415d58
Johannes Schumann <johannes.schumann@8me.de>
2018-04-24 14:02:07 +0200

    Set channel method added

--- ducroy/osci_control.py
+++ ducroy/osci_control.py
@@ -20,7 +20,7 @@ class Osci(object):
             self.visa_interface.write(command)
         except:
             print("Invalid combination of commands!")
-    
+
     def read(self, command, channel=None):
         if channel is not None:
             command = channel + ":" + command
@@ -30,3 +30,17 @@ class Osci(object):
         except:
             print("Couldn't read command!")
             return None
+
+    def set_channel_gain(self, voltage, channel):
+        command = "VDIV"
+        voltage_string = self.decimal_to_visa_string(voltage)
+        self.write(command, channel, voltage_string, "V")
+        readback = self.read(command,channel)
+        readback = readback.replace(channel+":"+command,"")
+        readback = readback.replace("V","")
+        return float(readback)
+
+
+    def decimal_to_visa_string(self, value):
+        value = float(value)
+        return "{:.2E}".format(value)
