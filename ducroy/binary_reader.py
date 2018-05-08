d5c028ad40a52c14f7154c7037877bd93c4eb739
Tamas Gal <tgal@km3net.de>
2018-05-08 17:04:19 +0200

    Refactoring of legacy binary reader

--- ducroy/binary_reader.py
+++ ducroy/binary_reader.py
@@ -1,165 +1,53 @@
+"""
+Binary data parser for LeCroy waverunner.
+
+"""
 from struct import unpack
 import numpy as np
-import struct
 
 
-def read_timetrace(filename, N):
+def read_timetrace(filename):
     '''
     Read binary waveform file (.trc) from LeCroy waverunner
     Based on Matlab file LCREAD.m
-    Input:
-        filename : filename, string
-    Output:
+
+    Parameters
+    ----------
+    filename : str
+
+    Returns
+    -------
+    x, y, v_gain, v_off, h_int, h_off
 
     '''
-
     fid = open(filename, "rb")
     data = fid.read(50).decode('ascii')
-    WAVEDESC = str.find(data, 'WAVEDESC')
+    offset = str.find(data, 'WAVEDESC')
+    fid.seek(offset + 32, 0)
 
-    aCOMM_TYPE = WAVEDESC + 32
-    aCOMM_ORDER = WAVEDESC + 34
-    aWAVE_DESCRIPTOR = WAVEDESC + 36  # length of the descriptor block
-    aUSER_TEXT = WAVEDESC + 40  # length of the usertext block
-    aTRIGTIME_ARRAY = WAVEDESC + 48  # length of the TRIGTIME array
-    aWAVE_ARRAY_1 = WAVEDESC + 60  # length (in Byte) of the sample array
+    COMM_TYPE = unpack('<h', fid.read(2))  # noqa
+    fmt = '>' if ord(fid.read(1)) == 0 else '<'  # endianness
+    fid.read(1)
+    wave_descriptor = unpack(fmt + 'l', fid.read(4))[0]
+    data = unpack(fmt + '6l', fid.read(6*4))
+    user_text, _, trigtime_array, _, _, wave_array_1 = data
+    fid.read(48)
+    res1, res2, wave_array_count = unpack(fmt + 'hhl', fid.read(8))
+    fid.read(24)
+    subarray_count, _, _, v_gain, v_off = unpack(fmt + '3lff', fid.read(5*4))
+    fid.read(10)
+    nom_subarray_count, h_int, h_off = unpack(fmt + 'hfd', fid.read(2+4+8))
 
-    aRESERVED1 = WAVEDESC + 112
-    # aRESERVED2 = WAVEDESC + 114
-    # number of datapoints in data array
-    # (should be == WAVE_ARRAY_1 for "byte" style)
-    aWAVE_ARRAY_COUNT = WAVEDESC + 116
-    # (real?) number of sequences (= subarrays)
-    aSUBARRAY_COUNT = WAVEDESC + 144
+    if nom_subarray_count == 0:
+        subarray_count = res1 * res2
+        wave_array_1 = wave_array_count  # TOTAL number of datapoints
 
-    aVERTICAL_GAIN = WAVEDESC + 156
-    aVERTICAL_OFFSET = WAVEDESC + 160
+    fid.seek(offset + wave_descriptor + user_text + trigtime_array, 0)
+    y = np.frombuffer(fid.read(wave_array_1), 'b', wave_array_1)
+    x = np.arange(1, len(y) + 1)  # *h_int + h_off
 
-    # nominal number of sequences (= subarrays)
-    aNOM_SUBARRAY_COUNT = WAVEDESC + 174
-
-    aHORIZ_INTERVAL = WAVEDESC + 176
-    aHORIZ_OFFSET = WAVEDESC + 180
-
-    # determine the number storage format
-    # HIFIRST / LOFIRST    (big endian / little endian)
-
-    fid.seek(aCOMM_ORDER)
-    COMM_ORDER = ord(fid.read(1))
-    # print("aCOMM_ORDER: {}".format(aCOMM_ORDER))
-    # print("COMM_ORDER: {}".format(COMM_ORDER))
-    if COMM_ORDER == 0:
-        fmt = '>'
-    else:
-        fmt = '<'
-    # print("fmt: {}".format(fmt))
-    COMM_TYPE = ReadWord(fid, fmt, aCOMM_TYPE)
-    print("COMM_TYPE: {}".format(COMM_TYPE))
-    WAVE_DESCRIPTOR = ReadLong(fid, fmt, aWAVE_DESCRIPTOR)
-    print("WAVE_DESCRIPTOR: {}".format(WAVE_DESCRIPTOR))
-    USER_TEXT = ReadLong(fid, fmt, aUSER_TEXT)
-    print("USER_TEXT: {}".format(USER_TEXT))
-    TRIGTIME_array = ReadLong(fid, fmt, aTRIGTIME_ARRAY)
-    print("TRIGTIME_array: {}".format(TRIGTIME_array))
-    WAVE_ARRAY_1 = ReadLong(fid, fmt, aWAVE_ARRAY_1)
-    print("WAVE_ARRAY_1: {}".format(WAVE_ARRAY_1))
-    SUBARRAY_COUNT = ReadLong(fid, fmt, aSUBARRAY_COUNT)
-    print("SUBARRAY_COUNT: {}".format(SUBARRAY_COUNT))
-    VERTICAL_GAIN = ReadFloat(fid, fmt, aVERTICAL_GAIN)
-    print("VERTICAL_GAIN: {}".format(VERTICAL_GAIN))
-    try:
-        VERTICAL_OFFSET = ReadFloat(fid, fmt, aVERTICAL_OFFSET)
-    except:  # noqa
-        # print("No vertical offset found in: {}".format(filename))
-        VERTICAL_OFFSET = np.nan
-    # print("VERTICAL_OFFSET: {}".format(VERTICAL_OFFSET))
-    NOM_SUBARRAY_COUNT = ReadWord(fid, fmt, aNOM_SUBARRAY_COUNT)
-    # print("NOM_SUBARRAY_COUNT: {}".format(NOM_SUBARRAY_COUNT))
-    if NOM_SUBARRAY_COUNT == 0:
-        res1 = ReadWord(fid, fmt, aRESERVED1)
-        res2 = ReadWord(fid, fmt, aRESERVED1)
-        WAVE_ARRAY_COUNT = ReadLong(fid, fmt, aWAVE_ARRAY_COUNT)
-        print("Wave_ARRAY_COUNT: {}".format(WAVE_ARRAY_COUNT))
-        SUBARRAY_COUNT = res1 * res2   # TOTAL number of sequences
-        WAVE_ARRAY_1 = WAVE_ARRAY_COUNT  # TOTAL number of datapoints
-    HORIZ_INTERVAL = ReadFloat(fid, fmt, aHORIZ_INTERVAL)
-    HORIZ_OFFSET = ReadDouble(fid, fmt, aHORIZ_OFFSET)
-    fid.close()
-    # print(data, WAVEDESC, COMM_ORDER, COMM_TYPE, WAVE_DESCRIPTOR,
-    # USER_TEXT, TRIGTIME_array, WAVE_ARRAY_1)
-    fid = open(filename, "rb")
-    y = np.array([])
-    for i in range(N):
-        y1 = ReadData(fid, fmt,
-                      WAVEDESC + WAVE_DESCRIPTOR + USER_TEXT + TRIGTIME_array +
-                      i * WAVE_ARRAY_1, WAVE_ARRAY_1)   # voltage [counts]
-
-        y = np.append(y, y1)
-    x = np.arange(1, len(y) + 1)  # *HORIZ_INTERVAL + HORIZ_OFFSET
     fid.close()
 
-    return (x.reshape(SUBARRAY_COUNT * N, WAVE_ARRAY_1 // SUBARRAY_COUNT),
-            y.reshape(SUBARRAY_COUNT * N, WAVE_ARRAY_1 // SUBARRAY_COUNT),
-            VERTICAL_GAIN,
-            VERTICAL_OFFSET,
-            HORIZ_INTERVAL,
-            HORIZ_OFFSET)
-
-
-def ReadByte(fid, fmt, Addr):
-    fid.seek(Addr)
-    s = fid.readline(1)
-    s = unpack(fmt + 'b', s)
-    if(type(s) == tuple):
-        return s[0]
-    else:
-        return s
-
-
-def ReadWord(fid, fmt, Addr):
-    fid.seek(Addr)
-    s = fid.readline(2)
-    s = unpack(fmt + 'h', s)
-    if(type(s) == tuple):
-        return s[0]
-    else:
-        return s
-
-
-def ReadLong(fid, fmt, Addr):
-    fid.seek(Addr)
-    s = fid.readline(4)
-    s = unpack(fmt + 'l', s)
-    if(type(s) == tuple):
-        return s[0]
-    else:
-        return s
-
-
-def ReadFloat(fid, fmt, Addr):
-    fid.seek(Addr)
-    s = fid.readline(4)
-    s = unpack(fmt + 'f', s)
-    if(type(s) == tuple):
-        return s[0]
-    else:
-        return s
-
-
-def ReadDouble(fid, fmt, Addr):
-    fid.seek(Addr)
-    s = fid.readline(8)
-    s = unpack(fmt + 'd', s)
-    if(type(s) == tuple):
-        return s[0]
-    else:
-        return s
-
-
-def ReadData(fid, fmt, Addr, datalen):
-    fid.seek(Addr)
-    fmt = fmt + str(datalen) + "b"
-    nbytes = struct.calcsize(fmt)
-    data = fid.read(nbytes)
-    result = np.frombuffer(data, 'b', nbytes)
-    return result
+    return (x.reshape(subarray_count, wave_array_1 // subarray_count),
+            y.reshape(subarray_count, wave_array_1 // subarray_count),
+            v_gain, v_off, h_int, h_off)
--- ducroy/tests/test_binary_reader.py
+++ ducroy/tests/test_binary_reader.py
@@ -13,20 +13,20 @@ TESTFILENAME = os.path.join(CWD, "test.trc")
 class TestBinaryReader(unittest.TestCase):
     def test_call_with_invalid_filename(self):
         with self.assertRaises(FileNotFoundError):
-            read_timetrace('foo', 1)
+            read_timetrace('foo')
 
     def test_call_with_valid_data(self):
-        read_timetrace(TESTFILENAME, 1)
+        read_timetrace(TESTFILENAME)
 
     def test_output(self):
-        x, y, v_gain, v_off, h_int, h_off = read_timetrace(TESTFILENAME, 1)
+        x, y, v_gain, v_off, h_int, h_off = read_timetrace(TESTFILENAME)
         self.assertAlmostEqual(0.00884, v_gain, 5)
         self.assertAlmostEqual(0.61, v_off, 5)
         self.assertAlmostEqual(2e-10, h_int, 11)
         self.assertAlmostEqual(1.35e-7, h_off, 8)
 
     def test_output_x(self):
-        x, y, v_gain, v_off, h_int, h_off = read_timetrace(TESTFILENAME, 1)
+        x, y, v_gain, v_off, h_int, h_off = read_timetrace(TESTFILENAME)
         assert 500 == len(x)
         assert np.allclose([1, 2, 3, 4, 5, 6, 7], x[0, :7])
         assert np.allclose([1003, 1004, 1005, 1006, 1007], x[1, :5])
@@ -34,7 +34,7 @@ class TestBinaryReader(unittest.TestCase):
         assert np.allclose([51, 52], x[0, 50:52])
 
     def test_output_y(self):
-        x, y, v_gain, v_off, h_int, h_off = read_timetrace(TESTFILENAME, 1)
+        x, y, v_gain, v_off, h_int, h_off = read_timetrace(TESTFILENAME)
         assert 500 == len(y)
         assert np.allclose([70., 69., 68., 70., 70., 70., 71.], y[0, :7])
         assert np.allclose([71., 71., 70., 71., 71., 71., 69.], y[1, :7])
